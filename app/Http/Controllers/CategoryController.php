7b8037c6001f070b315079f26d56eb228a72fc3c
Anya <korolevaanna2025@gmail.com>
2026-01-09 13:41:33 +0400

    2 test

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -100,6 +100,7 @@ class CategoryController extends Controller
 
         $category = Category::find($id);
         $category->delete();
+
         return redirect(route('categories.index'));
     }
 }
